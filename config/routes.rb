3032189922eaaa4a6c4b9784e67edd6613dc579a
Chad Williams <chadgit13@gmail.com>
2018-11-05 17:04:09 +0000

    Hello route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'directory/results'
 
+  root 'application#hello'
+
   resources :students
   resources :classrooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
